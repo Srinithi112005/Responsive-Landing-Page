250239af73474a424b28d8905670456d6eb06af5
Srinithi112005 <rsrinithisabitha@gmail.com>
2024-12-28 18:30:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function changeBg()
+{
+    var navbar = document.getElementById('navbar');
+    var scroll = window.scrollY;
+    if (scroll < 200) {
+        navbar.classList.remove('BgColour');
+    } else {
+        navbar.classList.add('BgColour');
+    }
+}
+
+window.addEventListener('scroll', scrollValue);
